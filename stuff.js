8557d29a1fe14ee14a06b21c8913951bc40df2bb
otaviocarvalho <ocarvalh@thoughtworks.com>
2018-07-26 16:26:47 -0300

    Add text shadow with Phong rendering and lights

--- stuff.js
+++ stuff.js
@@ -1,19 +1,19 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 30, window.innerWidth/window.innerHeight, 0.1, 1500 );
+var camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 1500);
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({alpha: true});
 renderer.setSize( window.innerWidth *0.9 , window.innerHeight * 0.9);
 document.body.appendChild( renderer.domElement );
 
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 , wireframe: true} );
+var material = new THREE.MeshPhongMaterial( { color: 0xB22222, flatShading: true } );
 var textMesh = null;
 var group = null;
 
 var loader = new THREE.FontLoader();
 
-loader.load('node_modules/three/examples/fonts/optimer_regular.typeface.json', function (font) {
+loader.load('node_modules/three/examples/fonts/droid/droid_sans_regular.typeface.json', function (font) {
 
-  var text = new THREE.TextGeometry('ORRRRAAMEU', {
+  var text = new THREE.TextGeometry('Ágil não manda e-mail', {
     font: font,
     size: 70,
     height: 20,
@@ -32,7 +32,13 @@ loader.load('node_modules/three/examples/fonts/optimer_regular.typeface.json', f
   group.position.y = 100;
 
   scene.add(group);
-  
+
+  var dirLight = new THREE.DirectionalLight( 0xffffff, 0.125 );
+  dirLight.position.set( 0, 0, 1 ).normalize();
+  scene.add( dirLight );
+  var pointLight = new THREE.PointLight( 0xffffff, 1.5 );
+  pointLight.position.set( 0, 100, 90 );
+  scene.add( pointLight );
 
   animate();
 
